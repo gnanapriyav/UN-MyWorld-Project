d3f13d8ae6af6093c1f581c4826663d093c2b7c5
ubuntu <priya@ubuntu.(none)>
2014-05-06 22:52:14 -0400

    added graphs_analysis.R under Visuals folder

--- Visuals/Graphs_Analysis.R
+++ Visuals/Graphs_Analysis.R
@@ -66,3 +66,85 @@ ggplot(p111,aes(y=priority111,x=Indicator.Description, fill=Impact)) + geom_bar(
 ggplot(p113,aes(y=priority113,x=Indicator.Description, fill=Impact)) + geom_bar() + coord_flip() + ggtitle("Freedom from discrimination and persecution Vs Beta Coefficient") + ylab("Beta Coefficient") + xlab("World Bank Indicator")  + theme(axis.text=element_text(size=12,face="bold"), axis.title=element_text(size=14,face="bold"))                                                                                                           
 ggplot(p114,aes(y=priority114,x=Indicator.Description, fill=Impact)) + geom_bar() + coord_flip() + ggtitle("Equality between men and women Vs Beta Coefficient") + ylab("Beta Coefficient") + xlab("World Bank Indicator")  + theme(axis.text=element_text(size=12,face="bold"), axis.title=element_text(size=14,face="bold"))                                                                                                           
 ggplot(p115,aes(y=priority115,x=Indicator.Description, fill=Impact)) + geom_bar() + coord_flip() + ggtitle("Better job opportunities Vs Beta Coefficient") + ylab("Beta Coefficient") + xlab("World Bank Indicator")  + theme(axis.text=element_text(size=12,face="bold"), axis.title=element_text(size=14,face="bold"))
+
+
+#Create circular graph
+t0<- data.frame(Indicator=as.character(p100[1:5,1]))
+t1<- data.frame(Indicator=as.character(p101[1:5,1]))
+t2<- data.frame(Indicator=as.character(p102[1:5,1]))
+t3<- data.frame(Indicator=as.character(p103[1:5,1]))
+t4<- data.frame(Indicator=as.character(p104[1:5,1]))
+t5<- data.frame(Indicator=as.character(p105[1:5,1]))
+t6<- data.frame(Indicator=as.character(p106[1:5,1]))
+t7<- data.frame(Indicator=as.character(p107[1:5,1]))
+t8<- data.frame(Indicator=as.character(p108[1:5,1]))
+t9<- data.frame(Indicator=as.character(p109[1:5,1]))
+t10<- data.frame(Indicator=as.character(p110[1:5,1]))
+t11<- data.frame(Indicator=as.character(p111[1:5,1]))
+t12<- data.frame(Indicator=as.character(p112[1:5,1]))
+t13<- data.frame(Indicator=as.character(p113[1:5,1]))
+t14<- data.frame(Indicator=as.character(p114[1:5,1]))
+t15<- data.frame(Indicator=as.character(p115[1:5,1]))
+
+#Bind t0 thru t15 into Ind dataframe
+Ind <- unique(rbind(t0,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15))
+Ind$Indicator.Description <- as.character(Ind$Indicator.Description)
+coeff$Indicator.Description <- as.character(coeff$Indicator.Description)
+circle_data <- join(coeff,Ind,by="Indicator.Description",type="inner")
+#backup circle_data
+circle_data_b <- circle_data
+
+#Replace all negative values to zero
+circle_data[circle_data<0] <- 0
+
+#sum up the coefficients of priorities for each of the indicators
+circle_data$sum <- rowSums(circle_data[,3:18])
+#calculate the sum of circle_data$sum
+s <- sum(circle_data$sum)
+#calcuate the width of each indicator within 75% of the total circumference of the circle
+circle_data$widthpercentage <- (circle_data$sum/s) * 75
+#sanity check -- sum(circle_data$widthpercentage) = 75
+
+#create columns with width percentage for the edges between each of the indicators and the priorities. 
+circle_data$p100widthpercentage <- (circle_data[,3] / circle_data$widthpercentage) * 100
+#For example -- circle_data$p100widthpercentage of corresponding (circle_data$widthpercentage * 0.75) = x. where x is the width of the edge between specific indicator and the priority
+circle_data$p101widthpercentage <- (circle_data[,4] / circle_data$widthpercentage) * 100
+circle_data$p102widthpercentage <- (circle_data[,5] / circle_data$widthpercentage) * 100
+circle_data$p103widthpercentage <- (circle_data[,6] / circle_data$widthpercentage) * 100
+circle_data$p104widthpercentage <- (circle_data[,7] / circle_data$widthpercentage) * 100
+circle_data$p105widthpercentage <- (circle_data[,8] / circle_data$widthpercentage) * 100
+circle_data$p106widthpercentage <- (circle_data[,9] / circle_data$widthpercentage) * 100
+circle_data$p107widthpercentage <- (circle_data[,10] / circle_data$widthpercentage) * 100
+circle_data$p108widthpercentage <- (circle_data[,11] / circle_data$widthpercentage) * 100
+circle_data$p109widthpercentage <- (circle_data[,12] / circle_data$widthpercentage) * 100
+circle_data$p110widthpercentage <- (circle_data[,13] / circle_data$widthpercentage) * 100
+circle_data$p111widthpercentage <- (circle_data[,14] / circle_data$widthpercentage) * 100
+circle_data$p112widthpercentage <- (circle_data[,15] / circle_data$widthpercentage) * 100
+circle_data$p113widthpercentage <- (circle_data[,16] / circle_data$widthpercentage) * 100
+circle_data$p114widthpercentage <- (circle_data[,17] / circle_data$widthpercentage) * 100
+circle_data$p115widthpercentage <- (circle_data[,18] / circle_data$widthpercentage) * 100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
